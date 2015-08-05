d0e43285227495607ea8a3d4d8d253381100ce63
Dave Lockhart <dave.lockhart@d2l.com>
2015-08-05 13:26:20 -0400

    Using style service font family & size and implemented basic "loading" functionality

--- src/app.js
+++ src/app.js
@@ -10,10 +10,66 @@ System.config({
 	}
 });
 
-function setProperty(id, value) {
-	document.getElementById(id).appendChild(
-		document.createTextNode(value)
-	);
+function createProperty(label, value) {
+	const labelNode = document.createElement('strong');
+	labelNode.appendChild(document.createTextNode(label + ': '));
+	const liNode = document.createElement('li');
+	liNode.appendChild(labelNode);
+	liNode.appendChild(document.createTextNode(value));
+	document.getElementById('props').appendChild(liNode);
+}
+
+function initOrgunit(client) {
+	return client.request('orgUnit').then((orgUnit) => {
+		createProperty('org id', orgUnit.OrgId);
+		createProperty('org unit id', orgUnit.OrgUnitId);
+	});
+}
+
+function initLang(client) {
+	return localeProvider.getLangTag().then((langTag) => {
+		document.getElementsByTagName('html')[0]
+			.setAttribute('lang', langTag);
+		createProperty('locale', langTag);
+	});
+}
+
+function initRtl(client) {
+	return localeProvider.isRtl().then((isRtl) => {
+		if(isRtl) {
+			document.body.dir = 'rtl';
+		}
+		createProperty('right-to-left', isRtl.toString());
+	});
+}
+
+function installStyles(client) {
+	return client.getService('style', '0.1')
+		.then((style) => { return style.getFont(); })
+		.then((font) => {
+			document.body.style.fontFamily = font.family;
+			document.body.style.fontSize= font.size;
+			createProperty('font family', font.family);
+			createProperty('font size', font.size);
+		});
+}
+
+function whoami(client, request) {
+	return new Promise((resolve, reject) => {
+		client.request('valenceHost').then((valenceHost) => {
+			createProperty('valence host', valenceHost);
+			request.get(valenceHost + '/d2l/api/lp/1.0/users/whoami')
+				.withCredentials()
+				.use(auth)
+				.end((err,res) => {
+					document.getElementById('whoami')
+						.appendChild(document.createTextNode(
+							err ? err : res.text
+						));
+					resolve();
+				});
+		});
+	});
 }
 
 Promise.all([
@@ -22,35 +78,19 @@ Promise.all([
 ]).then((modules) => {
 	const ifrau = modules[0],
 		request = modules[1],
-		langTag = modules[2],
 		client = new ifrau.Client();
-	localeProvider.getLangTag().then((langTag) => {
-		document.getElementsByTagName('html')[0]
-			.setAttribute('lang', langTag);
-		setProperty('locale', langTag);
-	});
-	localeProvider.isRtl().then((isRtl) => {
-		if(isRtl) {
-			document.body.dir = 'rtl';
-		}
-		setProperty('rtl', isRtl.toString());
-	});
 	client
 		.connect()
 		.then(() => {
 			client.setTitle('hello world iframe');
-			client.request('orgUnit').then((orgUnit) => {
-				setProperty('orgId', orgUnit.OrgId);
-				setProperty('orgUnitId', orgUnit.OrgUnitId);
-			});
-			client.request('valenceHost').then((valenceHost) => {
-				setProperty('valenceHost', valenceHost);
-				request.get(valenceHost + '/d2l/api/lp/1.0/users/whoami')
-					.withCredentials()
-					.use(auth)
-					.end((err,res) => {
-						setProperty('whoami', err ? err : res.text);
-					});
+			Promise.all([
+				initOrgunit(client),
+				initLang(client),
+				initRtl(client),
+				whoami(client, request),
+				installStyles(client)
+			]).then(() => {
+				document.getElementById('content').style.display = 'block';
 			});
 		});
 	});
