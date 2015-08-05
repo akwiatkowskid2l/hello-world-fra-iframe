0ca938740a0ce870d3befab77edfa4d9fdee58c5
Dave Lockhart <dave.lockhart@d2l.com>
2015-08-05 14:15:26 -0400

    syntax highlighting everything

--- src/app.js
+++ src/app.js
@@ -10,63 +10,14 @@ System.config({
 	}
 });
 
-function createProperty(label, value) {
-	const labelNode = document.createElement('strong');
-	labelNode.appendChild(document.createTextNode(label + ': '));
-	const liNode = document.createElement('li');
-	liNode.appendChild(labelNode);
-	liNode.appendChild(document.createTextNode(value));
-	document.getElementById('props').appendChild(liNode);
-}
-
-function initOrgunit(client) {
-	return client.request('orgUnit').then((orgUnit) => {
-		createProperty('org id', orgUnit.OrgId);
-		createProperty('org unit id', orgUnit.OrgUnitId);
-	});
-}
-
-function initLang(client) {
-	return localeProvider.getLangTag().then((langTag) => {
-		document.getElementsByTagName('html')[0]
-			.setAttribute('lang', langTag);
-		createProperty('locale', langTag);
-	});
-}
-
-function initRtl(client) {
-	return localeProvider.isRtl().then((isRtl) => {
-		if(isRtl) {
-			document.body.dir = 'rtl';
-		}
-		createProperty('right-to-left', isRtl.toString());
-	});
-}
-
-function installStyles(client) {
-	return client.getService('style', '0.1')
-		.then((style) => { return style.getFont(); })
-		.then((font) => {
-			document.body.style.fontFamily = font.family;
-			document.body.style.fontSize= font.size;
-			createProperty('font family', font.family);
-			createProperty('font size', font.size);
-		});
-}
-
 function whoami(client, request) {
 	return new Promise((resolve, reject) => {
 		client.request('valenceHost').then((valenceHost) => {
-			createProperty('valence host', valenceHost);
 			request.get(valenceHost + '/d2l/api/lp/1.0/users/whoami')
 				.withCredentials()
 				.use(auth)
 				.end((err,res) => {
-					document.getElementById('whoami')
-						.appendChild(document.createTextNode(
-							err ? err : res.text
-						));
-					resolve();
+					resolve(err ? err : JSON.parse(res.text));
 				});
 		});
 	});
@@ -84,13 +35,42 @@ Promise.all([
 		.then(() => {
 			client.setTitle('hello world iframe');
 			Promise.all([
-				initOrgunit(client),
-				initLang(client),
-				initRtl(client),
-				whoami(client, request),
-				installStyles(client)
-			]).then(() => {
+				client.request('orgUnit'),
+				localeProvider.getLangTag(),
+				localeProvider.isRtl(),
+				client.getService('style', '0.1')
+					.then((style) => { return style.getFont(); }),
+				whoami(client, request)
+			]).then((ifrauData) => {
+
+				var langTag = ifrauData[1];
+				document.getElementsByTagName('html')[0]
+					.setAttribute('lang', langTag);
+
+				var isRtl = ifrauData[2];
+				if(isRtl) {
+					document.body.dir = 'rtl';
+				}
+
+				var font = ifrauData[3];
+				document.body.style.fontFamily = font.family;
+				document.body.style.fontSize= font.size;
+
+				var data = {
+					orgUnit: ifrauData[0],
+					langTag,
+					isRtl,
+					font,
+					whoAmI: ifrauData[4]
+				};
+				var code = document.getElementById('ifrauData');
+				code.appendChild(document.createTextNode(
+						JSON.stringify(data, null, '    ')
+					));
+				hljs.highlightBlock(code);
+
 				document.getElementById('content').style.display = 'block';
+
 			});
 		});
 	});
